a85cdcb9fbe55ee910505bcfae5fab066189ded8
Renan Machado <renan.mav@hotmail.com>
2019-06-09 03:59:43 -0300

    Setup react-navigation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '~/config/reactotron';
+import '~/config/reactdevtools';
+
+import React from 'react';
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Login = () => (
+  <View>
+    <Text>Login page</Text>
+  </View>
+);
+
+export default Login;
