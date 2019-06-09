d709a887cb12250cfa5beab2916edd94a2c366a8
Renan Machado <renan.mav@hotmail.com>
2019-06-09 04:22:03 -0300

    Global styles

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
+import styles from './styles';
 
 const Login = () => (
-  <View>
+  <View style={styles.container}>
     <Text>Login page</Text>
   </View>
 );
