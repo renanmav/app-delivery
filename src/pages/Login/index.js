aa9893e7c2235e6c64f85f8981eefd2a26b64f57
Renan Machado <renan.mav@hotmail.com>
2019-06-11 16:07:56 -0300

    Change Login statefull to functional

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,11 @@
-import React, { Component } from "react";
-
-import { StatusBar } from "react-native";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Types as AuthActions } from "~/store/ducks/auth";
 
 import background from "~/assets/background.png";
 import logo from "~/assets/logo.png";
 
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { Creators as AuthActions } from "~/store/ducks/auth";
-
+import { StatusBar } from "react-native";
 import {
   Container,
   Input,
@@ -23,74 +20,66 @@ import {
 
 import { colors } from "~/styles";
 
-class Login extends Component {
-  state = { email: "", password: "" };
+export default function Login() {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
-  handleSubmit = () => {
-    const { email, password } = this.state;
-    const { loginRequest } = this.props;
-    loginRequest(email, password);
+  const { loading, error } = useSelector(state => state.auth);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    const { email, password } = credentials;
+
+    dispatch({
+      type: AuthActions.LOGIN_REQUEST,
+      payload: { email, password }
+    });
   };
 
-  render() {
-    const { email, password } = this.state;
-    const { loading, error } = this.props.auth;
-
-    return (
-      <Background source={background}>
-        <Container>
-          <StatusBar color={colors.primary} />
-          <Logo source={logo} />
-          <Input
-            value={email}
-            onChangeText={email => this.setState({ email })}
-            autoCapitalize="none"
-            autoCorrect={false}
-            placeholder="Seu e-mail"
-            returnKeyType={"next"}
-            onSubmitEditing={() => {
-              this.secondTextInput.focus();
-            }}
-            blurOnSubmit={false}
-          />
-          <Input
-            value={password}
-            onChangeText={password => this.setState({ password })}
-            autoCapitalize="none"
-            autoCorrect={false}
-            placeholder="Sua senha secreta"
-            secureTextEntry
-            ref={input => {
-              this.secondTextInput = input;
-            }}
-            onSubmitEditing={this.handleSubmit}
-          />
-          <Button onPress={this.handleSubmit}>
-            {loading ? <ActivityIndicator /> : <TextButton>Entrar</TextButton>}
-          </Button>
-          {error ? (
-            <TextError>
-              Algo deu errado! Verifique suas credenciais ou tente novamente
-              mais tarde.
-            </TextError>
-          ) : null}
-          <CreateAccount>
-            <TextButton>Criar conta gratuita</TextButton>
-          </CreateAccount>
-        </Container>
-      </Background>
-    );
-  }
+  return (
+    <Background source={background}>
+      <Container>
+        <StatusBar color={colors.primary} />
+        <Logo source={logo} />
+        <Input
+          value={credentials.email}
+          onChangeText={email => setCredentials({ ...credentials, email })}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Seu e-mail"
+          returnKeyType={"next"}
+          onSubmitEditing={() => {
+            this.secondTextInput.focus();
+          }}
+          blurOnSubmit={false}
+        />
+        <Input
+          value={credentials.password}
+          onChangeText={password =>
+            setCredentials({ ...credentials, password })
+          }
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Sua senha secreta"
+          secureTextEntry
+          ref={input => {
+            this.secondTextInput = input;
+          }}
+          onSubmitEditing={handleSubmit}
+        />
+        <Button onPress={handleSubmit}>
+          {loading ? <ActivityIndicator /> : <TextButton>Entrar</TextButton>}
+        </Button>
+        {error ? (
+          <TextError>
+            Algo deu errado! Verifique suas credenciais ou tente novamente mais
+            tarde.
+          </TextError>
+        ) : null}
+        <CreateAccount>
+          <TextButton>Criar conta gratuita</TextButton>
+        </CreateAccount>
+      </Container>
+    </Background>
+  );
 }
-
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(AuthActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Login);
