1647fedf19cbce11285a62f89742434a5327cb1d
Renan Machado <renan.mav@hotmail.com>
2019-06-12 11:59:19 -0300

    Add currency formatter to Sizes page

--- src/pages/Sizes/list.js
+++ src/pages/Sizes/list.js
@@ -9,6 +9,8 @@ import { ContainerList, Size, SizeFile, SizeName, SizePrice } from "./styles";
 
 const { width } = Dimensions.get("window");
 
+import { TextMask } from "react-native-masked-text";
+
 const ListSizes = ({ sizes }) => (
   <ContainerList>
     {sizes.map((size, index) => (
@@ -23,7 +25,21 @@ const ListSizes = ({ sizes }) => (
           source={{ uri: `${api.baseURL}/files?name=${size.file.file}` }}
         />
         <SizeName>{size.name}</SizeName>
-        <SizePrice>{size.price}</SizePrice>
+        <SizePrice>
+          <TextMask
+            value={size.price}
+            type={"money"}
+            options={{
+              precision: 2,
+              separator: ",",
+              delimiter: ".",
+              unit: "R$",
+              suffixUnit: ""
+            }}
+          >
+            {size.price}
+          </TextMask>
+        </SizePrice>
       </Size>
     ))}
   </ContainerList>
